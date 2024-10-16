52af410c9ab4f73a1710019a70ce1f30e845379a
Bhargav <>
2024-10-16 09:34:23 +0530

    Edit TextSticker text.

--- app/src/main/java/com/textsticker/MainActivity.java
+++ app/src/main/java/com/textsticker/MainActivity.java
@@ -1,15 +1,18 @@
 package com.textsticker;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.InputType;
 import android.text.Layout;
 import android.util.DisplayMetrics;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    TextStickerView textSticker;
     int canvasWidth, canvasHeight;
 
     @Override
@@ -33,26 +36,26 @@ public class MainActivity extends AppCompatActivity {
 
     private void createTextStickView(int i, String str, Font font, int i2, int i3, float f, float f2, int i4, int i5,
                                      Layout.Alignment alignment, int i6, boolean z, boolean z2, float f3, float f4) {
-        this.textSticker = new TextStickerView(this, this.canvasWidth, this.canvasHeight);
+        TextStickerView textSticker = new TextStickerView(this, this.canvasWidth, this.canvasHeight);
         RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(-2, -2);
         layoutParams.addRule(10);
-        this.textSticker.setLayoutParams(layoutParams);
-        this.textSticker.setText(str);
-        this.textSticker.setLayoutX(i2);
-        this.textSticker.setLayoutY(i3);
-        this.textSticker.setRotateAngle(f);
-        this.textSticker.setScale(f2);
-        this.textSticker.setPaddingLeft(i4);
-        this.textSticker.setPaddingRight(i5);
-        this.textSticker.setFont(font);
-        this.textSticker.setAlign(alignment);
-        this.textSticker.setOpacity(i6);
-        this.textSticker.setUnderLine(z);
-        this.textSticker.setStrikethrough(z2);
-        this.textSticker.setLetterSpacing(f3);
-        this.textSticker.setLineSpacing(f4);
-        this.textSticker.setTag(Integer.valueOf(i));
-        this.textSticker.setOperationListener(new TextStickerView.OperationListener() {
+        textSticker.setLayoutParams(layoutParams);
+        textSticker.setText(str);
+        textSticker.setLayoutX(i2);
+        textSticker.setLayoutY(i3);
+        textSticker.setRotateAngle(f);
+        textSticker.setScale(f2);
+        textSticker.setPaddingLeft(i4);
+        textSticker.setPaddingRight(i5);
+        textSticker.setFont(font);
+        textSticker.setAlign(alignment);
+        textSticker.setOpacity(i6);
+        textSticker.setUnderLine(z);
+        textSticker.setStrikethrough(z2);
+        textSticker.setLetterSpacing(f3);
+        textSticker.setLineSpacing(f4);
+        textSticker.setTag(Integer.valueOf(i));
+        textSticker.setOperationListener(new TextStickerView.OperationListener() {
             public void onUnselect(TextStickerView textStickerView) {
             }
 
@@ -62,9 +65,7 @@ public class MainActivity extends AppCompatActivity {
             }
 
             public void onEdit(TextStickerView textStickerView) {
-                if (textStickerView.isInEditMode())
-                    textStickerView.setInEdit(false);
-                else textStickerView.setInEdit(true);
+                editSticker(textStickerView);
             }
 
             public void onTouch(TextStickerView textStickerView) {
@@ -78,5 +79,24 @@ public class MainActivity extends AppCompatActivity {
         });
         ((RelativeLayout) findViewById(R.id.rlMain)).addView(textSticker);
         ((RelativeLayout) findViewById(R.id.rlMain)).requestLayout();
+    }
+
+    private void editSticker(TextStickerView textStickerView) {
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        input.setText(textStickerView.getText());
+        input.setPadding(80,40,40,40);
+        input.requestFocus();
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Enter text")
+                .setView(input)
+                .setPositiveButton("OK", (dialog, which) -> {
+                    String userInput = input.getText().toString();
+                    textStickerView.setText(userInput);
+                    textStickerView.invalidate();
+                })
+                .setNegativeButton("Cancel", (dialog, which) -> dialog.cancel());
+        builder.show();
     }
 }
